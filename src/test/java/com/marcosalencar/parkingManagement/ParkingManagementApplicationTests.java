bb3f7219d18c7f0af82a5e25d7fbeff3babc1713
marcos alencar <marcos.12.01.07@gmail.com>
2025-06-04 15:58:09 -0300

    first commit

---
+++ src/test/java/com/marcosalencar/parkingManagement/ParkingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosalencar.parkingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
